b287ad2819163b4c59383ab1d701b8651d3a1626
Jack Kill <>
2023-01-05 11:37:53 -0600

    Got everything working; waiting to render until response received, and api call and receive works on ios emulator

--- frontend/api-practice/App.js
+++ frontend/api-practice/App.js
@@ -1,5 +1,6 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import React, { useState, useEffect } from 'react';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 
 // function to store userID value globally across app
@@ -12,28 +13,59 @@ const storeUserID = async (value) => {
 	}
 }
 
+// function to make API call
+function requestUser(userID) {
+  return new Promise(resolve => {
+    setTimeout(() => {
+      const response = fetch(`http://localhost:8080/api/users?id=${userID}`);
+      resolve(response);
+    }, 2000);
+  });
+}
+
 // function to use the stored userID to retrieve a JSON containing information
 async function getUser() {
   console.log("Retrieving userID...");
 	const userID = await AsyncStorage.getItem('userID');
-  console.log("Retrieved userID...",userID);
+  console.log("Retrieved userID...");
   console.log("Sending API request...");
-	const response = await fetch(`http://localhost:8080/api/users?id=${userID}`);
-  console.log("Awaiting API response...");
+	// const response = await fetch(`http://localhost:8080/api/users?id=${userID}`);
+  const response = await requestUser(userID);
 	const user = await response.json();
+  console.log("Response recieved: ", user);
   console.log("Retrieved API response...");
 	return user;
 }
 
 // App Rendering
 export default function App() {
-  storeUserID('000000000000001');
-  let user = getUser();
-  //figure out how to wait until this is done
-  //I'm so close- api returns valid json, react just renders too early
-  //maybe there's a way to wait until done, or live render?
-  console.log(user);
-  console.log(user.ID);
+  const [user, setUser] = useState();
+  const [isLoading, setLoading] = useState(true);
+
+  const prepUserData = () => {
+    storeUserID('837645918231');
+    console.log("Starting getUser Process...");
+    console.log("===========================");
+    getUser().then(response => {
+      setUser(response);
+      console.log("===========================");
+      console.log("Ending getUser Process...");
+      setLoading(false);
+    });
+  };
+
+  useEffect(() => {
+    prepUserData();
+  }, []);
+
+  if (isLoading) {
+    return (
+      <View className="App">
+        <Text>Loading...</Text>
+      </View>
+    );
+  }
+
   return (
     <View style={styles.container}>
       <Text>ID: {user.id} </Text>
